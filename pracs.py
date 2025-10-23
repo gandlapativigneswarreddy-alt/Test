dc87886b5d7ef6493e68fcbaff440184714369a1
gandlapativigneswarreddy-alt <gandlapativigneswarreddy@gmail.com>
2025-10-23 18:23:04 +0530

    update pracs

--- pracs.py
+++ pracs.py
@@ -1,5 +1,19 @@
-a =[1,2,3]
-b =a
+#a =[1,2,3]
+#b =a
 
 
-print (a is b) 
+#print (a is b) 
+
+a = 3
+b = 3
+c = 8
+if a > b and c:
+    print(a)
+elif b > a and c :
+    print(b) 
+elif c > a and b:
+    print(c)       
+else:
+    print("Equal")                   
+    
+     
